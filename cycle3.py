78770383bf2fafbb2c84b86f535068d99d52f89d
Jackson Phillips <60822114+Jacksonmj77@users.noreply.github.com>
2025-04-28 12:28:18 -0500

    added main page and attempted to link opencv to mainpage to run

--- cycle3.py
+++ cycle3.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, Response, jsonify, render_template
 import os
 import base64
 from datetime import datetime
+from camera_opencv import Camera
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -12,6 +13,26 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/mainpage')
+def mainpage():
+    return render_template('mainpage.html')
+
+
+def gen(camera):
+    "Video streaming generator function."
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route('/upload_photo', methods=['POST'])
 def upload_photo():
